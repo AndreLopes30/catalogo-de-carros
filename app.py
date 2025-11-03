ab801d7ccb2c3f5be23a79079d5fd120f3e48bc0
AndreLopes30 <ferreiraandre833@gmail.com>
2025-11-03 19:40:32 -0300

    Adiocando banco dedados SQLite

--- app.py
+++ app.py
@@ -1,27 +1,20 @@
 from flask import Flask, render_template, request, redirect, send_file
 from fpdf import FPDF
-import json
+import sqlite3
 import os
 import io
 
 app = Flask(__name__)
-ARQUIVO_CARROS = 'carros.json'
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/catalogo', methods=['GET', 'POST'])
 def catalogo():
-
-    if os.path.exists(ARQUIVO_CARROS):
-        with open(ARQUIVO_CARROS, 'r', encoding='utf-8') as f:
-            carros = json.load(f)
-    else:
-        carros = []
+    conn = sqlite3.connect('carros.db')
+    cursor = conn.cursor()
 
     if request.method == 'POST':
-        # Pegando os valores do formulário
         modelo = request.form['modelo']
         ano = int(request.form['ano'])
         preco = int(request.form['preco'])
@@ -32,48 +25,20 @@ def catalogo():
 
             
 
-        carro = {
-            'modelo': modelo,
-            'ano': ano,
-            'preco': preco,
-            'imagem': imagem
-        }
-
-        carros.append(carro)
-        with open(ARQUIVO_CARROS, 'w', encoding='utf-8') as f:
-            json.dump(carros, f, indent=4, ensure_ascii=False)
-
+        cursor.execute("""
+            INSERT INTO carros (modelo, ano, preco, imagem)
+            VALUES (?, ?, ?, ?)
+        """, (modelo, ano, preco, imagem))
+        conn.commit()
+        conn.close()
         return redirect('/catalogo')
 
+    cursor.execute("SELECT modelo, ano, preco, imagem FROM carros")
+    carros = [
+    {'modelo': modelo, 'ano': ano, 'preco': preco, 'imagem': imagem}
+    for modelo, ano, preco, imagem in cursor.fetchall()]
+    conn.close()
     return render_template('catalogo.html', carros=carros)
-
-@app.route('/relatorio')
-def relatorio():
-    with open(ARQUIVO_CARROS, 'r', encoding='utf-8') as f:
-        carros = json.load(f)
-
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font("Arial", size=12)
-    pdf.cell(200, 10, txt="Relatório de Carros", ln=True, align='C')
-
-    for carro in carros:
-        pdf.cell(200, 10, txt=f"Modelo: {carro['modelo']} - Ano: {carro['ano']} - Preço: R$ {carro['preco']}", ln=True)
-
-    pdf_output = pdf.output(dest='S').encode('latin1')
-
-    pdf_buffer = io.BytesIO(pdf_output)
-
-    return send_file(
-        pdf_buffer,
-        as_attachment=True,
-        download_name="relatorio_carros.pdf",
-        mimetype='application/pdf'
-    )
-
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
