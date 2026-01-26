b9b3691322a372c0e7589bfec3454eb5395ce005
AndreLopes30 <ferreiraandre833@gmail.com>
2026-01-26 13:38:02 -0300

    Adição de endpoint

--- app.py
+++ app.py
@@ -13,19 +13,25 @@ def catalogo():
     conn = sqlite3.connect('carros.db')
     cursor = conn.cursor()
 
-    cursor.execute("SELECT modelo, ano, preco, imagem FROM carros")
+    cursor.execute("SELECT id, modelo, ano, preco, imagem FROM carros")
     carros = [
-        {'modelo': modelo, 'ano': ano, 'preco': preco, 'imagem': imagem}
-        for modelo, ano, preco, imagem in cursor.fetchall()
-    ]
+    {
+        'id': id,
+        'modelo': modelo,
+        'ano': ano,
+        'preco': preco,
+        'imagem': imagem
+    }
+    for id, modelo, ano, preco, imagem in cursor.fetchall()
+]
 
     if request.method == 'POST':
         modelo = request.form['modelo']
         ano = int(request.form['ano'])
-        preco = int(request.form['preco'])
+        preco = float(request.form['preco'])
         imagem = request.form['imagem']
 
-        if modelo == '' or ano < 1900 or ano > 2025 or preco < 0:
+        if modelo == '' or ano < 1900 or ano > 2026 or preco < 0:
             conn.close()
             return render_template('index.html', carros=carros, erro="Algum dos campos inválido.")
 
@@ -40,7 +46,33 @@ def catalogo():
     conn.close()
     return render_template('catalogo.html', carros=carros)
 
+@app.route('/carro/<int:id>')
+def infos(id):
+    conn = sqlite3.connect('carros.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT id, modelo, ano, preco, imagem
+        FROM carros
+        WHERE id = ?
+    """, (id,))
+
+    row = cursor.fetchone()
+    conn.close()
+
+    if row is None:
+        return "Carro não encontrado", 404
+
+    carro = {
+        'id': row[0],
+        'modelo': row[1],
+        'ano': row[2],
+        'preco': row[3],
+        'imagem': row[4]
+    }
+
+    return render_template('carro.html', carro=carro)
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
+    app.run(host="0.0.0.0", port=port, debug=True)
