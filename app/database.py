7916b145963afdc6be2040d56d7f17f895368a17
AndreLopes30 <ferreiraandre833@gmail.com>
2026-02-13 09:27:50 -0300

    Ajuste de db

--- app/database.py
+++ app/database.py
@@ -1,7 +1,11 @@
 import sqlite3
+import os
+
+base_dir = os.path.abspath(os.path.dirname(__file__) + "/..")
+db_path = os.path.join(base_dir, 'carros.db')
 
 def init_db():
-    with sqlite3.connect('carros.db') as conn:
+    with sqlite3.connect(db_path) as conn:
         conn.execute('''
             CREATE TABLE IF NOT EXISTS carros (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
